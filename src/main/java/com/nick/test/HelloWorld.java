4ec974992fd6a1a366275bfd0a5f0d360bd5e6a5
Nick <1374175412@qq.com>
2023-08-02 10:08:12 +0800

    hello world
    Signed-off-by: Nick <1374175412@qq.com>

---
+++ src/main/java/com/nick/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.nick.test;
+
+/**
+ * @author Nick
+ * @Classname HelloWorld
+ * @Date 2023/08/02 10:06
+ * @Description TODO
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+}
